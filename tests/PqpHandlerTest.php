14e9c0c9943b3f039283abef1967936b0c2a63e1
jacobemerick <jpemeric@gmail.com>
2016-01-14 05:57:40 -0700

    Adds more tests to construct and formatter

--- tests/PqpHandlerTest.php
+++ tests/PqpHandlerTest.php
@@ -2,8 +2,11 @@
 
 namespace Jacobemerick\MonologPqp;
 
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
 use Particletree\Pqp\Console;
 use PHPUnit_Framework_TestCase;
+use ReflectionClass;
 
 class PqpHandlerTest extends PHPUnit_Framework_TestCase
 {
@@ -11,7 +14,55 @@ class PqpHandlerTest extends PHPUnit_Framework_TestCase
     public function testIsInstanceOfPqpHandler()
     {
         $pqpHandler = new PqpHandler(new Console());
-echo 'hi';
         $this->assertInstanceOf('Jacobemerick\MonologPqp\PqpHandler', $pqpHandler);
+    }
+
+    public function testConstructSetsParent()
+    {
+        $pqpHandler = new PqpHandler(new Console(), Logger::NOTICE, false);
+
+        $this->assertAttributeEquals(Logger::NOTICE, 'level', $pqpHandler);
+        $this->assertAttributeEquals(false, 'bubble', $pqpHandler);
+    }
+
+    public function testConstructSetsDefaults()
+    {
+        $pqpHandler = new PqpHandler(new Console());
+
+        $this->assertAttributeEquals(Logger::DEBUG, 'level', $pqpHandler);
+        $this->assertAttributeEquals(true, 'bubble', $pqpHandler);
+    }
+
+    public function testConstructSetsConsole()
+    {
+        $console = new Console();
+        $pqpHandler = new PqpHandler($console);
+
+        $this->assertAttributeInstanceOf('Particletree\Pqp\Console', 'console', $pqpHandler);
+        $this->assertAttributeSame($console, 'console', $pqpHandler);
+    }
+
+    public function testDefaultFormatterInstanceOfLineFormatter()
+    {
+        $reflectedHandler = new ReflectionClass('Jacobemerick\MonologPqp\PqpHandler');
+        $reflectedFormatterMethod = $reflectedHandler->getMethod('getDefaultFormatter');
+        $reflectedFormatterMethod->setAccessible(true);
+
+        $pqpHandler = new PqpHandler(new Console());
+        $formatter = $reflectedFormatterMethod->invoke($pqpHandler);
+
+        $this->assertInstanceOf('Monolog\Formatter\LineFormatter', $formatter);
+    }
+
+    public function testDefaultFormatterSetsFormat()
+    {
+        $reflectedHandler = new ReflectionClass('Jacobemerick\MonologPqp\PqpHandler');
+        $reflectedFormatterMethod = $reflectedHandler->getMethod('getDefaultFormatter');
+        $reflectedFormatterMethod->setAccessible(true);
+
+        $pqpHandler = new PqpHandler(new Console());
+        $formatter = $reflectedFormatterMethod->invoke($pqpHandler);
+
+        $this->assertAttributeEquals("%channel%.%level_name%: %message%\n", 'format', $formatter);
     }
 }
